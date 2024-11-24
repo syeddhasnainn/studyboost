84e1307da3ee76e287a77280cb1ece92ba8d432d
syeddhasnainn <syedhasnainmurtaza@gmail.com>
2024-11-24 22:51:19 +0000

    added metadata index

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -3,11 +3,12 @@ import { getCloudflareContext } from "@opennextjs/cloudflare";
 
 export async function POST(request: NextRequest) {
   const { env } = await getCloudflareContext();
-  const { messages } = (await request.json()) as {
+  const { messages, resourceId } = (await request.json()) as {
     messages: { role: string; content: string }[];
+    resourceId: string;
   };
+
   const singleMessage = messages[messages.length - 1].content;
-  console.log("singleMessage", singleMessage);
 
   const embeddingResponse = await env.AI.run("@cf/baai/bge-small-en-v1.5", {
     text: singleMessage,
@@ -15,12 +16,31 @@ export async function POST(request: NextRequest) {
 
   const embeddings = embeddingResponse.data[0];
 
-  const queryEmbeddings = await env.VECTORIZE.query(embeddings);
-  console.log("queryEmbeddings", queryEmbeddings);
-
-  const response = await env.AI.run("@cf/meta/llama-3.1-8b-instruct", {
-    messages,
+  const queryEmbeddings = await env.VECTORIZE.query(embeddings, {
+    topK: 2,
+    filter: {
+      resourceId: resourceId,
+    },
+    returnMetadata: "all",
   });
+
+
+  const context = queryEmbeddings.matches
+    .map((m) => m.metadata?.text)
+    .join(" ");
+  const systemPrompt = {
+    role: "system",
+    content: `You are a helpful assistant. You have to answer the question based on the following context: ${context}`,
+  };
+
+  messages.unshift(systemPrompt);
+
+  const response = await env.AI.run(
+    "@hf/nousresearch/hermes-2-pro-mistral-7b",
+    {
+      messages,
+    } as BaseAiTextGeneration["inputs"]
+  );
 
   return NextResponse.json({ response });
 }
