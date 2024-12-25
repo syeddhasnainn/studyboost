c7c278315378e2e5a5026af2435d48b87cc3ccb9
syeddhasnainn <syedhasnainmurtaza@gmail.com>
2024-12-25 21:03:43 +0000

    save

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,28 @@
 import NextAuth, { type DefaultSession } from "next-auth"
 
 import Google from "next-auth/providers/google"
+
+
+declare module "next-auth" {
+  /**
+   * Returned by `auth`, `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user: {
+      /** The user's id. */
+      id: string
+      /**
+       * By default, TypeScript merges new interface properties and overwrites existing ones.
+       * In this case, the default session user properties will be overwritten,
+       * with the new ones defined above. To keep the default session user properties,
+       * you need to add them back into the newly declared interface.
+       */
+    } & DefaultSession["user"]
+  }
+}
+
+
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Google({
