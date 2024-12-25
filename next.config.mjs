668dd5e832e0ee6f9c162446923f5ba9b31d3f06
syeddhasnainn <syedhasnainmurtaza@gmail.com>
2024-12-25 17:39:43 +0000

    wip

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,12 @@ const nextConfig = {
     serverComponentsExternalPackages: ["canvas", "pdfjs-dist"],
   },
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    WORKOS_API_KEY: process.env.WORKOS_API_KEY,
+    WORKOS_CLIENT_ID: process.env.WORKOS_CLIENT_ID,
+    WORKOS_COOKIE_PASSWORD: process.env.WORKOS_COOKIE_PASSWORD,
+    NEXT_PUBLIC_WORKOS_REDIRECT_URI: process.env.NEXT_PUBLIC_WORKOS_REDIRECT_URI
+
   },
 };
 
